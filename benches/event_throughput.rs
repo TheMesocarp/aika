427a9fb5bffa91d12734ddcc2051bf2e8ead197c
mooshi <sushi@fibered.cat>
2025-07-11 19:24:05 -0400

    deps

--- benches/event_throughput.rs
+++ benches/event_throughput.rs
@@ -4,7 +4,8 @@ use aika::{
     messages::Msg,
     st::World,
 };
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use std::hint::black_box;
 
 // Minimal agent that just schedules timeout events
 struct ThroughputAgent {
